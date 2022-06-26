c41b563cff987220977383957725032e268576d1
Isaque Almeida <isaque.santos@ufpe.br>
2022-06-26 18:11:32 -0300

    Adiciona exercício do bloco 8.2

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/hof.js
@@ -0,0 +1,74 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// PROCURAR A AUTORA DO LIVRO QUE NASCEU EM 1947:
+const autor = books.find((book) => book.author.birthYear === 1947).author.name
+/* console.log(autor); */
+
+// RETORNAR TRUE SE TODOS NASCERAM NO SÉCULO XX:
+const seculoVinte = books.every((nascido) => nascido.author.birthYear >= 1900 ? true : false)
+// console.log(seculoVinte);
+
+// RETORNA TRUE SE ALGUM LIVRO FOI LANÇADO NA DÉCADA DE 80 E FALSE SE NÃO:
+const livro = books.some((book) => book.releaseYear >= 1980 && book.releaseYear <= 1989? true : false);
+console.log(livro);
